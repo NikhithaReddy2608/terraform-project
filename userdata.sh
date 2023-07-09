bb5496732963bce0f485ea08c48d7c6abdeada32
NikhithaReddy2608 <nellinikhithareddy@gmail.com>
2023-07-09 15:18:20 +0530

    userdata

---
+++ userdata.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "The page was created by the user data" | sudo tee /var/www/html/index.html
+sudo git clone https://github.com/amolshete/card-website.git
+
+sudo cp -rf card-website/* /var/www/html/
+
